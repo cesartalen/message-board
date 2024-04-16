6a2c2569d8790a81e806c142f1afce7d1fb4ab53
cesartalen <cesartalense@gmail.com>
2024-04-16 15:11:23 +0200

    initial, clerk, middleware

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,25 +1,25 @@
 import { sql } from "drizzle-orm";
 import {
   index,
+  integer,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
 
-export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
+export const createTable = pgTableCreator((name) => `messageboard_${name}`);
 
 export const posts = createTable(
   "post",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    userId: varchar("user_id", { length: 256 }),
+    userName: varchar("user_name", {length: 256}),
+    message: varchar("name", { length: 1024 }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+  }
+)
